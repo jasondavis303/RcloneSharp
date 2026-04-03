5736af073d473e9486551739dd84d3e02928491e
Jason Davis <jasondavis303@gmail.com>
2026-04-03 11:01:27 -0600

    Update version info

--- RcloneSharp/VersionInfo.cs
+++ RcloneSharp/VersionInfo.cs
@@ -3,13 +3,13 @@
 public static class VersionInfo
 {
     /// <summary>
-    /// The version of Rclone released at the time this lib was released
+    /// The version of Rclone used in development at the time this lib was released
     /// </summary>
-    public const string RcloneVersion = "v1.69.3";
+    public const string RcloneVersion = "v1.71.1";
 
 
     /// <summary>
     /// The date this library was last released
     /// </summary>
-    public const string AsOfDate = "2025-06-07";
+    public const string AsOfDate = "2026-04-03";
 }
